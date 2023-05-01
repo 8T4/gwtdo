6ac701ded0386c8318daf62c4cc03526d0583ac3
yanjustino <yanlimaj@gmail.com>
2023-05-01 12:44:52 -0300

    Update README.md

--- src/Samples/Gwtdo.Sample.XUnit/StocksTests.cs
+++ src/Samples/Gwtdo.Sample.XUnit/StocksTests.cs
@@ -19,7 +19,7 @@ public class StocksTests : Feature<TradingContext, TradingFixture>, IClassFixtur
     public void test_with_extension_methods()
     {
         FeatureContext.Setup();
-        
+
         GIVEN
             .I_have_100_shares_of_MSFT_stock().And
             .I_have_150_shares_of_APPL_stock();
@@ -29,11 +29,45 @@ public class StocksTests : Feature<TradingContext, TradingFixture>, IClassFixtur
             .I_should_have_80_shares_of_MSFT_stock().And
             .I_should_have_150_shares_of_APPL_stock().And
             .A_sell_order_for_20_shares_of_MSFT_stock_should_have_been_executed();
-    }    
-    
+    }
+
     [Fact]
     [Scenario(@"User requests a sell before close of trading")]
-    public async Task test_with_attribute_mapping()
+    public void test_with_attribute_mapping()
+    {
+        Describe("When an asset is sold",
+            GIVEN
+            | "I have 100 shares of MSFT stock" | AND
+            | "I have 150 shares of APPL stock" | AND
+            | "The time is before close of trading" |
+            WHEN
+            | "I ask to sell 20 shares of MSFT stock" |
+            THEN
+            | "I should have 80 shares of MSFT stock" | AND
+            | "I should have 150 shares of APPL stock" | AND
+            | "A sell order for 20 shares of MSFT stock should have been executed");
+    }
+
+    [Fact]
+    [Scenario(@"User requests a sell before close of trading")]
+    public async Task test_with_attribute_mappingAsync()
+    {
+        await DescribeAsync("When an asset is bought",
+            GIVEN
+            | "I have 100 shares of MSFT stock" | AND
+            | "I have 150 shares of APPL stock" | AND
+            | "The time is before close of trading" |
+            WHEN
+            | "I ask to buy 20 shares of MSFT stock" |
+            THEN
+            | "I should have 120 shares of MSFT stock" | AND
+            | "I should have 150 shares of APPL stock" | AND
+            | "A sell order for 0 shares of MSFT stock should have been executed");
+    }
+
+    [Fact]
+    [Scenario(@"User buy and requests a sell before close of trading")]
+    public async Task test_two_steps_with_attribute_mappingAsync()
     {
         await DescribeAsync("When an asset is sold",
             GIVEN
@@ -46,7 +80,7 @@ public class StocksTests : Feature<TradingContext, TradingFixture>, IClassFixtur
             | "I should have 80 shares of MSFT stock" | AND
             | "I should have 150 shares of APPL stock" | AND
             | "A sell order for 20 shares of MSFT stock should have been executed");
-        
+
         await DescribeAsync("When an asset is bought",
             GIVEN
             | "I have 100 shares of MSFT stock" | AND
@@ -58,7 +92,7 @@ public class StocksTests : Feature<TradingContext, TradingFixture>, IClassFixtur
             | "I should have 120 shares of MSFT stock" | AND
             | "I should have 150 shares of APPL stock" | AND
             | "A sell order for 0 shares of MSFT stock should have been executed");
-    }    
+    }
 
     [Theory]
     [InlineData(100, 20, 80, "MSFT")]
