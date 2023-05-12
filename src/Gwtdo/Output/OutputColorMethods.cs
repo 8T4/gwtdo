30e8b060cbedfdb46c6389af6005648188831143
yanjustino <yanlimaj@gmail.com>
2023-05-12 12:36:23 -0300

    ajuste console

--- src/Gwtdo/Output/OutputColorMethods.cs
+++ src/Gwtdo/Output/OutputColorMethods.cs
@@ -8,29 +8,45 @@ namespace Gwtdo.Output;
 internal static class OutputColorMethods
 {
     internal static string Error(this string value) =>
-        System.Console.IsOutputRedirected
-            ? @$"\u001b[0m\u001b[31m{value}\u001b[0m"
-            : value.ColorString(ConsoleColor.Red).Value;
+        Write(
+            @$"\u001b[0m\u001b[31m{value}\u001b[0m",
+            value.ColorString(ConsoleColor.Red).Value
+        );
 
     internal static string Success(this string value) =>
-        System.Console.IsOutputRedirected
-            ? @$"\u001b[0m\u001b[32m{value}\u001b[0m"
-            : value.ColorString(ConsoleColor.Green).Value;
+        Write(
+            $"\\u001b[32m{value}\\u001b[0m",
+            value.ColorString(ConsoleColor.Green).Value
+        );
 
     internal static string Warning(this string value) =>
-        System.Console.IsOutputRedirected
-            ? @$"\u001b[0m\u001b[33m{value}\u001b[0m"
-            : value.ColorString(ConsoleColor.Yellow).Value;
+        Write(
+            @$"\u001b[0m\u001b[33m{value}\u001b[0m",
+            value.ColorString(ConsoleColor.Yellow).Value
+        );
 
     internal static string Reset(this string value) =>
-        System.Console.IsOutputRedirected
-            ? @$"\u001b[0m\u001b[0m{value}\u001b[0m"
-            : value.ColorString(ConsoleColor.White).Value;
+        Write(
+            @$"\u001b[0m\u001b[0m{value}\u001b[0m",
+            value.ColorString(ConsoleColor.White).Value
+        );
+
+    private static string Write(string colorUnix, string colorWindows)
+    {
+        return Console.IsOutputRedirected
+            ? Environment.OSVersion.Platform switch
+            {
+                PlatformID.MacOSX => colorUnix,
+                PlatformID.Unix => colorUnix,
+                _ => colorWindows
+            }
+            : colorWindows;
+    }
 
     private static OutputRichString ColorString(this string value, ConsoleColor color)
     {
         if (string.IsNullOrEmpty(value)) return new OutputRichString(string.Empty);
-        
+
         var colorChar = OutputColorData.ConsoleColorToUnicode[color];
         if (value[0] >= '\uE000')
         {
@@ -38,5 +54,5 @@ internal static class OutputColorMethods
         }
 
         return new OutputRichString($"{colorChar}{value}{colorChar}");
-    }    
+    }
 }
